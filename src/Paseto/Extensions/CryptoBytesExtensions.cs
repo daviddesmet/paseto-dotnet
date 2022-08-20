5de50ae87774fdf749259afc721f431d25b23bcc
Timothy Makkison <timmakkison@gmail.com>
2022-08-20 20:28:39 +0100

    Added CryptoBytesExtensions

---
+++ src/Paseto/Extensions/CryptoBytesExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Paseto.Extensions;
+
+using System;
+using System.Runtime.CompilerServices;
+
+public static class CryptoBytesExtensions
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    internal static void InternalWipe(Span<byte> data, int offset, int count) => data.Slice(offset, count).Clear();
+    public static void Wipe(Span<byte> data) => InternalWipe(data, 0, data.Length);
+}
