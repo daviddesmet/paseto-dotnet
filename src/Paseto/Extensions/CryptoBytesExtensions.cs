ae969ea4931e929efd264421bd833caba1a2a88c
Timothy Makkison <timmakkison@gmail.com>
2022-08-21 11:11:45 +0100

    Edit change formatting

--- src/Paseto/Extensions/CryptoBytesExtensions.cs
+++ src/Paseto/Extensions/CryptoBytesExtensions.cs
@@ -7,5 +7,8 @@ public static class CryptoBytesExtensions
 {
     [MethodImpl(MethodImplOptions.NoInlining)]
     internal static void InternalWipe(Span<byte> data, int offset, int count) => data.Slice(offset, count).Clear();
+
     public static void Wipe(Span<byte> data) => InternalWipe(data, 0, data.Length);
+
+    public static void SpanCopy(Span<byte> sourceSpan, int sourceIndex, Span<byte> destinationSpan, int destinationIndex, int length) => sourceSpan.Slice(sourceIndex, length).CopyTo(destinationSpan.Slice(destinationIndex, length));
 }
