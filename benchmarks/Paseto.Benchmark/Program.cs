60bfb1e158ed11f2a3ab0f12cb0dea6f1711a0cb
Timothy Makkison <timmakkison@gmail.com>
2022-08-29 15:52:32 +0100

    Added benchmarks for V1-V4 for encrypt, decrypt, sign and verify

---
+++ benchmarks/Paseto.Benchmark/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Paseto;
+using Paseto.Builder;
+using Paseto.Cryptography.Key;
+using Xunit;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net50)]
+[SimpleJob(RuntimeMoniker.Net60)]
+[JsonExporterAttribute.Full]
+[JsonExporterAttribute.FullCompressed]
+public class Benchmarks
+{
+    private readonly byte[] _seed = new byte[32];
+    private PasetoSymmetricKey _symmetricKey = null!;
+    private PasetoAsymmetricKeyPair _asymmetricKeyPair = null!;
+    private string _localToken = null!;
+    private string _publicToken = null!;
+
+    [GlobalSetup]
+    public void SetUp()
+    {
+        _symmetricKey = new PasetoBuilder().Use(Version, Purpose.Local).GenerateSymmetricKey();
+        _asymmetricKeyPair = new PasetoBuilder().Use(Version, Purpose.Public).GenerateAsymmetricKeyPair(_seed);
+
+        _localToken = LocalEncode();
+        _publicToken = PublicEncode();
+    }
+
+    [ParamsAllValues]
+    public ProtocolVersion Version { get; set; }
+
+    // TODO Experiment with a large claim/many claims
+    public static PasetoBuilder CreateBuilder() => new PasetoBuilder().Issuer("localhost:5000")
+                                                                      .Subject("PASETO-DEMO")
+                                                                      .Audience("paseto.io")
+                                                                      .NotBefore(DateTime.UtcNow)
+                                                                      .IssuedAt(DateTime.UtcNow)
+                                                                      .Expiration(DateTime.UtcNow.AddHours(1))
+                                                                      .AddClaim("Test", "Value")
+                                                                      .AddClaim("Test2", "Value2");
+
+    [Benchmark]
+    public string LocalEncode() => CreateBuilder().Use(Version, Purpose.Local)
+                              .WithKey(_symmetricKey)
+                              .Encode();
+
+    [Benchmark]
+    public PasetoTokenValidationResult LocalDecode()
+    {
+        var result = CreateBuilder().Use(Version, Purpose.Local)
+                                    .WithKey(_symmetricKey)
+                                    .Decode(_localToken);
+
+        Assert.True(result.IsValid);
+        return result;
+    }
+
+    [Benchmark]
+    public string PublicEncode() => CreateBuilder().Use(Version, Purpose.Public)
+                                                   .WithKey(_asymmetricKeyPair.SecretKey)
+                                                   .Encode();
+
+    [Benchmark]
+    public PasetoTokenValidationResult PublicDecode()
+    {
+        var result = CreateBuilder().Use(Version, Purpose.Public)
+                                    .WithKey(_asymmetricKeyPair.PublicKey)
+                                    .Decode(_publicToken);
+
+        Assert.True(result.IsValid);
+        return result;
+    }
+}
