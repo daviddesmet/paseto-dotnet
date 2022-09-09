e29b056b14ba5b96f9991dcbb248a52e2001bbc4
Timothy Makkison <timmakkison@gmail.com>
2022-09-09 15:57:21 +0100

    Remove allocation

--- src/Paseto/Cryptography/Internal/Ed25519Ref10/keypair.cs
+++ src/Paseto/Cryptography/Internal/Ed25519Ref10/keypair.cs
@@ -1,23 +1,28 @@
 ﻿namespace Paseto.Cryptography.Internal.Ed25519Ref10;
 
 using System;
-using NaCl.Core.Internal;
+using Paseto.Extensions;
 
 internal static partial class Ed25519Operations
 {
-    internal static void crypto_sign_keypair(byte[] pk, int pkoffset, byte[] sk, int skoffset, byte[] seed, int seedoffset)
+    internal static void crypto_sign_keypair(Span<byte> pk, int pkoffset, Span<byte> sk, int skoffset, ReadOnlySpan<byte> seed, int seedoffset)
     {
         GroupElementP3 A;
         int i;
+        Span<byte> h = stackalloc byte[64];
 
-        Array.Copy(seed, seedoffset, sk, skoffset, 32);
-        var h = Sha512.Hash(sk, skoffset, 32); // TODO: Remove alloc
+        SpanExtensions.Copy(seed, seedoffset, sk, skoffset, 32);
+
+        var hasher = new Sha512();
+        hasher.Update(sk, skoffset, 32);
+        hasher.Finish(h);
+
         ScalarOperations.sc_clamp(h, 0);
 
         GroupOperations.ge_scalarmult_base(out A, h, 0);
         GroupOperations.ge_p3_tobytes(pk, pkoffset, ref A);
 
-        for (i = 0; i < 32; ++i) sk[skoffset + 32 + i] = pk[pkoffset + i];
-        CryptoBytes.Wipe(h);
+        SpanExtensions.Copy(pk, pkoffset, sk, skoffset+32, 32);
+        CryptoBytesExtensions.Wipe(h);
     }
 }
