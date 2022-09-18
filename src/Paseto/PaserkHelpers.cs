c16b6abbe37b4b0a6ebc57cb28d62f7e35b63060
Timothy Makkison <timmakkison@gmail.com>
2022-09-18 18:08:01 +0100

    Refactor SimpleDecode and SimpleEncode

--- src/Paseto/PaserkHelpers.cs
+++ src/Paseto/PaserkHelpers.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
-using System.Text.RegularExpressions;
 using Paseto;
 using Paseto.Cryptography.Key;
 using static Paseto.Utils.EncodingHelper;
@@ -9,29 +9,24 @@ internal static class PaserkHelpers
 {
     private const int SYM_KEY_SIZE_IN_BYTES = 32;
 
+    private const int V1_ASYM_MIN_PUBLIC_KEY_SIZE = 292;
+    private const int V1_ASYM_MIN_PRIVATE_KEY_SIZE = 1180;
+
     private const int V2V4_ASYM_PUBLIC_KEY_SIZE = 32;
     private const int V2V4_ASYM_PRIVATE_KEY_SIZE = 64;
+
     private const int V3_ASYM_MIN_PRIVATE_KEY_SIZE = 48;
 
     internal static string SimpleEncode(string header, PaserkType type, PasetoKey pasetoKey)
     {
-        if (pasetoKey.Protocol is not { Version: "v1" or "v2" or "v3" or "v4" })
-            throw new PaserkNotSupportedException($"The PASERK version {pasetoKey.Protocol.Version} is not supported");
+        var version = StringToVersion(pasetoKey.Protocol.Version);
+
         if (!Paserk.IsKeyCompatible(type, pasetoKey))
             throw new PaserkNotSupportedException($"The PASERK type is not compatible with the key {pasetoKey}.");
 
-        var length = pasetoKey.Key.Length;
-        return (type, pasetoKey.Protocol.Version, length) switch
-        {
-            (PaserkType.Local, _, not SYM_KEY_SIZE_IN_BYTES) => throw new ArgumentException($"The key length in bytes must be {SYM_KEY_SIZE_IN_BYTES}."),
+        ValidateKeyLength(type, version, pasetoKey.Key.Length);
 
-            (PaserkType.Public, "v2" or "v4", not V2V4_ASYM_PUBLIC_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be {V2V4_ASYM_PUBLIC_KEY_SIZE}."),
-            (PaserkType.Secret, "v2" or "v4", not V2V4_ASYM_PRIVATE_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be {V2V4_ASYM_PRIVATE_KEY_SIZE}."),
-
-            (PaserkType.Secret, "v3", < V3_ASYM_MIN_PRIVATE_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be at least {V3_ASYM_MIN_PRIVATE_KEY_SIZE}."),
-
-            _ => $"{header}{ToBase64Url(pasetoKey.Key.Span)}",
-        };
+        return $"{header}{ToBase64Url(pasetoKey.Key.Span)}";
     }
 
     internal static PasetoKey SimpleDecode(PaserkType type, ProtocolVersion version, string encodedKey)
@@ -39,112 +34,37 @@ internal static class PaserkHelpers
         var key = FromBase64Url(encodedKey);
         var protocolVersion = Paserk.CreateProtocolVersion(version);
 
-        return (type,version, key.Length) switch
+        ValidateKeyLength(type, version, key.Length);
+
+        return type switch
         {
-            (PaserkType.Local, _, not SYM_KEY_SIZE_IN_BYTES) => throw new ArgumentException($"The key length in bytes must be {SYM_KEY_SIZE_IN_BYTES}."),
+            PaserkType.Local => new PasetoSymmetricKey(key, protocolVersion),
+            PaserkType.Public => new PasetoAsymmetricPublicKey(key, protocolVersion),
+            PaserkType.Secret => new PasetoAsymmetricSecretKey(key, protocolVersion),
 
-            (PaserkType.Public, ProtocolVersion.V2 or ProtocolVersion.V4, not V2V4_ASYM_PUBLIC_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be {V2V4_ASYM_PUBLIC_KEY_SIZE}."),
-            (PaserkType.Secret, ProtocolVersion.V2 or ProtocolVersion.V4, not V2V4_ASYM_PRIVATE_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be {V2V4_ASYM_PRIVATE_KEY_SIZE}."),
-
-            (PaserkType.Secret, ProtocolVersion.V3, < V3_ASYM_MIN_PRIVATE_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be at least {V3_ASYM_MIN_PRIVATE_KEY_SIZE}."),
-
-            (PaserkType.Local,_,_) => new PasetoSymmetricKey(key, protocolVersion),
-            (PaserkType.Public,_,_) => new PasetoAsymmetricPublicKey(key, protocolVersion),
-            (PaserkType.Secret,_,_) => new PasetoAsymmetricSecretKey(key, protocolVersion),
-
-            //(PaserkType.Local, SYM_KEY_SIZE_IN_BYTES) => new PasetoSymmetricKey(key, protocolVersion),
-            //(PaserkType.Local,_) => throw new ArgumentException($"The key length in bytes must be {SYM_KEY_SIZE_IN_BYTES}."),
-
-            //(PaserkType.Secret, V2V4_ASYM_PRIVATE_KEY_SIZE)== V2V4_ASYM_PRIVATE_KEY_SIZE => new PasetoAsymmetricSecretKey(DecodePrivateKey(encodedKey), protocolVersion),
-            //PaserkType.Secret when key.Length == V2V4_ASYM_PRIVATE_KEY_SIZE => throw new ArgumentException($"The key length in bytes must be {V2V4_ASYM_PRIVATE_KEY_SIZE}."),
-
-            //PaserkType.Secret => new PasetoAsymmetricSecretKey(DecodePrivateKey(encodedKey), protocolVersion),
-            //PaserkType.Public => new PasetoAsymmetricPublicKey(DecodePublicKey(encodedKey), protocolVersion),
             _ => throw new PaserkInvalidException($"Error type {type} is not compatible with ${nameof(SimpleDecode)}"),
         };
     }
 
-
-    private static readonly Regex ECDsaPrivateKeyRegex = new(@"-----(BEGIN|END) EC PRIVATE KEY-----[\W]*", RegexOptions.Compiled);
-    private static readonly Regex RsaPrivateKeyRegex = new(@"-----(BEGIN|END) (RSA|OPENSSH|ENCRYPTED) PRIVATE KEY-----[\W]*", RegexOptions.Compiled);
-    private static readonly Regex RsaPublicKeyRegex = new(@"-----(BEGIN|END) PUBLIC KEY-----[\W]*", RegexOptions.Compiled);
-
-    internal static byte[] DecodePublicKey(string encodedKey)
+    internal static void ValidateKeyLength(PaserkType type, ProtocolVersion version, int length) => _ = (type, version, length) switch
     {
-        if (RsaPublicKeyRegex.IsMatch(encodedKey))
-        {
-            var rsaPublicKey = RSA.Create();
-#if NET5_0_OR_GREATER
-            rsaPublicKey.ImportFromPem(encodedKey);
-#elif NETCOREAPP3_1
-            var publicKeyBase64 = RsaPublicKeyRegex.Replace(key, "");
-            var publicKey = Convert.FromBase64String(publicKeyBase64);
-            rsaPublicKey.ImportRSAPublicKey(new ReadOnlySpan<byte>(publicKey), out _);
-#endif
+        (PaserkType.Local, _, not SYM_KEY_SIZE_IN_BYTES) => throw new ArgumentException($"The key length in bytes must be {SYM_KEY_SIZE_IN_BYTES}."),
 
-            //var pk = rsaPublicKey.ToCompatibleXmlString(false);
-            //return GetBytes(pk);
-            return rsaPublicKey.ExportRSAPublicKey();
-        }
+        (PaserkType.Public, ProtocolVersion.V1, < V1_ASYM_MIN_PUBLIC_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be at least {V1_ASYM_MIN_PUBLIC_KEY_SIZE}."),
+        (PaserkType.Public, ProtocolVersion.V2 or ProtocolVersion.V4, not V2V4_ASYM_PUBLIC_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be {V2V4_ASYM_PUBLIC_KEY_SIZE}."),
 
-        return FromBase64Url(encodedKey);
-    }
+        (PaserkType.Secret, ProtocolVersion.V1, < V1_ASYM_MIN_PRIVATE_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be at least {V1_ASYM_MIN_PRIVATE_KEY_SIZE}."),
+        (PaserkType.Secret, ProtocolVersion.V2 or ProtocolVersion.V4, not V2V4_ASYM_PRIVATE_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be {V2V4_ASYM_PRIVATE_KEY_SIZE}."),
+        (PaserkType.Secret, ProtocolVersion.V3, < V3_ASYM_MIN_PRIVATE_KEY_SIZE) => throw new ArgumentException($"The key length in bytes must be at least {V3_ASYM_MIN_PRIVATE_KEY_SIZE}."),
+        _ => 0,
+    };
 
-    internal static byte[] DecodePrivateKey(string encodedKey)
+    internal static ProtocolVersion StringToVersion(string version) => version switch
     {
-        if (ECDsaPrivateKeyRegex.IsMatch(encodedKey))
-        {
-            var ecdsaSecretKey = ECDsa.Create();
-            ecdsaSecretKey.ImportFromPem(encodedKey);
-            var sk = ecdsaSecretKey.ExportECPrivateKey();
-            return sk;
-
-            /*
-            using var ms = new MemoryStream(GetBytes(key));
-            using var sr = new StreamReader(ms);
-            var pemReader = new PemReader(sr);
-            var pem = pemReader.ReadPemObject();
-
-            var seq = Asn1Sequence.GetInstance(pem.Content);
-            var e = seq.GetEnumerator();
-            e.MoveNext();
-            var version = ((DerInteger)e.Current).Value;
-            if (version.IntValue == 0) // V1
-            {
-                var privateKeyInfo = PrivateKeyInfo.GetInstance(seq);
-                var akp = Org.BouncyCastle.Security.PrivateKeyFactory.CreateKey(privateKeyInfo);
-            }
-            else
-            {
-                var ec = Org.BouncyCastle.Asn1.Sec.ECPrivateKeyStructure.GetInstance(seq);
-                var algId = new AlgorithmIdentifier(Org.BouncyCastle.Asn1.X9.X9ObjectIdentifiers.IdECPublicKey, ec.GetParameters());
-                var privateKeyInfo = new PrivateKeyInfo(algId, ec.ToAsn1Object());
-                var der = privateKeyInfo.GetDerEncoded();
-                var akp = Org.BouncyCastle.Security.PrivateKeyFactory.CreateKey(privateKeyInfo);
-
-                return der;
-            }
-
-            return pem.Content; // same as sk
-            */
-        }
-
-        if (RsaPrivateKeyRegex.IsMatch(encodedKey))
-        {
-            var rsaSecretKey = RSA.Create();
-#if NET5_0_OR_GREATER
-            rsaSecretKey.ImportFromPem(encodedKey);
-#elif NETCOREAPP3_1
-            var privateKeyBase64 = RsaPrivateKeyRegex.Replace(key, "");
-            var privateKey = Convert.FromBase64String(privateKeyBase64);
-            rsaSecretKey.ImportRSAPrivateKey(new ReadOnlySpan<byte>(privateKey), out _);
-#endif
-
-            //var sk = rsaSecretKey.ToCompatibleXmlString(true);
-            //return GetBytes(sk);
-            return rsaSecretKey.ExportRSAPrivateKey();
-        }
-
-        return FromBase64Url(encodedKey);
-    }
+        "v1" => ProtocolVersion.V1,
+        "v2" => ProtocolVersion.V2,
+        "v3" => ProtocolVersion.V3,
+        "v4" => ProtocolVersion.V4,
+        _ => throw new PaserkNotSupportedException($"The PASERK version {version} is not recognised."),
+    };
 }
